1c37bd466042129d5106784efd139b56847af26a
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-25 02:06:35 +0200

    commit1_master

---
+++ UnitTest_7,2,it/UnitTest_7,2,it.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_7.2.іт/lab_7.2.іт.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest72it
+{
+	TEST_CLASS(UnitTest72it)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            int count = 0;
+            int s = 0;
+            int result;
+            const int rowCOUNT = 4;
+            const int colCOUNT = 4;
+            int a[rowCOUNT][colCOUNT] = {
+              {1, 5, 8, 10},
+              {2, 15, 7, 9},
+              {-1, -2, 4, 0},
+              {-3, 4, 6, 9}
+            };
+            int* pa[4] = { a[0], a[1], a[2], a[3] };
+            Max(pa, rowCOUNT, colCOUNT, );
+            result = count;
+            Assert::AreEqual(9, result);
+
+		}
+	};
+}
---
+++ lab_7.2.іт/lab_7.2.іт.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void Create(int** a, const int HIGH, const int LOW, const int rowCount, const int colCount)
+{
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+            a[i][j] = LOW + rand() % (HIGH - LOW + 1);
+    }
+}
+void Print(int** a, const int rowCount, const int colCount)
+{
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            cout << a[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void Change(int** a, int& imax, int& jmax, int& i, int& j, const int colCount)
+{
+    int tmp = 0;
+    tmp = a[imax][jmax];
+    a[imax][jmax] = a[i][i];
+    a[i][i] = tmp;
+}
+
+
+void Replace(int** a, const int rowCount, const int colCount, int& max, int& imax, int& jmax)
+{
+    for (int i = 0; i < rowCount; i++)
+    {
+        int max = a[i][0];
+        for (int j = 0; j < colCount; j++)
+        {
+            if (a[i][j] >= max)
+            {
+                max = a[i][j];
+                jmax = j;
+                imax = i;
+                
+            }
+            if (j == (colCount - 1))
+            {
+                cout << "a[" << imax << "][" << jmax << "] = " << max << endl;
+
+                if (a[i][i] != a[imax][jmax])
+                {
+                  Change(a, imax, jmax, i, j, colCount);
+                }
+            }
+        }
+    }
+}
+int Max(int** a, const int rowCount, const int colCount, int& max, int& imax_1, int& jmax_1)
+{
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            if (a[i][j] >= max )
+            {
+                max = a[i][j];
+                jmax_1 = j;
+                imax_1 = i;
+            }
+        }
+    }
+    return max;
+}
+
+
+
+int main()
+{
+    srand(time(NULL));
+
+    const int HIGH = 100;
+    const int LOW = 1;
+    int rowCount;
+    int colCount;
+    cout << "Enter the matrix size: "; cin >> rowCount;
+    colCount = rowCount;
+
+    int** a = new int* [rowCount];
+    for (int i = 0; i < rowCount; i++)
+        a[i] = new int[colCount];
+
+
+    Create(a, HIGH, LOW, rowCount, colCount);
+    Print(a, rowCount, colCount);
+    int max = a[0][0];
+    int imax = 0;
+    int jmax = 0;
+    Replace(a, rowCount, colCount, max, imax, jmax);
+
+    Print(a, rowCount, colCount);
+
+
+    for (int i = 0; i < rowCount; i++)
+        delete[] a[i];
+    delete[]a;
+
+
+    system("pause");
+    return 0;
+
+}
